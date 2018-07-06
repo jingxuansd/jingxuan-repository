650addc05847b58d482ff49fbaa6d94a99e14977
jingxuansd <360244939@qq.com>
2018-07-06 15:33:41 +0800

    first commit

---
+++ src/main/java/LearnMain.java
@@ -0,0 +1,37 @@
+import queue.BlockingQueue;
+
+/**
+ * Created by jingxuan on 2018/7/6.
+ */
+public class LearnMain {
+    private static BlockingQueue<Integer> queue = new BlockingQueue<Integer>(10);
+    public static void main(String[] args) throws InterruptedException {
+        Thread a = new Thread() {
+            @Override
+            public void run() {
+                for( int j = 0 ; j < 100 ; j++ )
+                    try {
+                        queue.put(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+            }
+        };
+        a.start();
+        Thread b = new Thread() {
+            @Override
+            public void run() {
+                for( int j = 0 ; j < 100 ; j++ )
+                    try {
+                        queue.take();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+            }
+        };
+        b.start();
+        a.join();
+        b.join();
+        System.out.println();
+    }
+}
